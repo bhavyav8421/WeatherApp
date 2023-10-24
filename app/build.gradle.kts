16c209f407ae1ec2a04a3290a4126cde5bf545eb
Bhavya Velayudhan <bhavyav8421@gmail.com>
2023-10-24 06:37:44 -0700

    Update build.gradle.kts : Removed commented code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,35 +76,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-   /* testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-    testImplementation("androidx.arch.core:core-testing:2.1.0")*/
-
-    /*testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:3.4.6")
-    testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")*/
-
-    /*testImplementation("androidx.arch.core:core-testing:2.2.0")
-    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.44.2")
-    testImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:core:1.5.0")
-
-    testImplementation("org.mockito:mockito-core:5.0.0")
-    androidTestImplementation("org.mockito:mockito-android:3.10.0")*/
-
-
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:3.4.6")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
